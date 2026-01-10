7c373362a5bddb7ac5db6ba7ada17c11717424b3
layesow <abdoulayesow716@gmail.com>
2026-01-10 19:17:15 +0000

    creation de composants creation de layout et header et footer

---
+++ src/components/layouts/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='w-full flex justify-center bg-gray-200 px-5 py-3 mt-5'>
+      <p className='text-center'>
+        &copy; 2024 E-commerce. All rights reserved.
+      </p>
+    </div>
+  )
+}
+
+export default Footer
