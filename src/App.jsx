5e0045f66c1a509cd72080053c4db051b3a14eb6
layesow <abdoulayesow716@gmail.com>
2026-01-10 19:17:15 +0000

    creation des pages et mise en structure de mon projet reation de route dans app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 import {  } from 'react'
-import Header from './components/layouts/Header'
-import Footer from './components/layouts/Footer'
 
 function App() {
 
   return (
     <>
-      <Header />
-      <div className='min-h-[80vh] flex flex-col items-center justify-center'>
-        <h2 className='text-3xl font-dmSans'>Welcome to the E-commerce Store</h2>
-      </div>
-      <Footer />
+      <h1 className='bg-amber-400'>Hello, World!</h1>
     </>
   )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../components/layouts/Header'
+import Footer from '../components/layouts/Footer'
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+        <h2>Homme</h2>
+      <Footer />
+    </>
+  )
+}
+
+export default Home
