a6c0fb19268071b030634984146960eb9ef4dd07
layesow <abdoulayesow716@gmail.com>
2026-01-22 16:16:41 +0000

    tet

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,16 @@
 import {  } from 'react'
 import Header from './components/layouts/Header'
 import Footer from './components/layouts/Footer'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home';
-import Shop from './pages/Shop';
-import Product from './pages/Product';
-import Register from './pages/Register';
-import Login from './pages/Login';
-import Cart from './pages/Cart';
-import Checkout from './pages/Checkout';
 
 function App() {
 
   return (
     <>
-      <Routes>
-          <Route path='/' element={<Home/>} />
-          <Route path='/shop' element={<Shop/>} />
-          <Route path='/product' element={<Product/>} />
-          <Route path='/login' element={<Login/>} />
-          <Route path='/register' element={<Register/>} />
-          <Route path='/cart' element={<Cart/>} />
-          <Route path='/checkout' element={<Checkout/>} />
-      </Routes>
+      <Header />
+      <div className='min-h-[80vh] flex flex-col items-center justify-center'>
+        <h2 className='text-3xl font-dmSans'>Welcome to the E-commerce Store</h2>
+      </div>
+      <Footer />
     </>
   )
 }
