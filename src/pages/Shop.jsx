3d5309653453ea62ba60bba7149330f4e59b150e
layesow <abdoulayesow716@gmail.com>
2026-01-24 17:58:12 +0000

    ceration de layout et mettre heder et footer et mettre le shop en place

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Layout from '../components/layouts/Layout'
+import { Link } from 'react-router-dom'
+import { IoIosArrowForward } from "react-icons/io";
+
 
 const Shop = () => {
   return (
-    <div>Shop</div>
+    <>
+      <Layout>
+        {/* breadcrump */}
+        <div className="max-w-360 lg:px-8 px-5 mx-auto">
+          <div className="flex items-center gap-x-2">
+            <Link>Home</Link>
+            <IoIosArrowForward />
+            <Link>Shop</Link>
+          </div>
+        </div>
+
+        {/* title */}
+
+        {/* main content */}
+      </Layout>
+    </>
   )
 }
 
